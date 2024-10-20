2642d990db2e9c98e1564d6f75b3fa15b352766f
dalsu0222 <dalsu0222@sju.ac.kr>
2024-10-20 15:59:37 +0900

    fix: JWT 토큰 쿠키 설정 보완

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 // controllers/authController.js
-const User = require('../modules/User');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const User = require("../modules/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const secret = process.env.JWT_SECRET;
 const saltRounds = 10;
 
@@ -25,7 +25,7 @@ const authController = {
   async login(req, res) {
     const { username, password } = req.body;
     const userDoc = await User.findOne({ username });
-    
+
     if (!userDoc) {
       return res.status(404).json({ message: "존재하지 않는 사용자입니다" });
     }
@@ -34,10 +34,19 @@ const authController = {
     if (passOk) {
       jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
         if (err) throw err;
-        res.cookie("token", token).json({
-          id: userDoc._id,
-          username,
-        });
+        res
+          .cookie("token", token, {
+            httpOnly: true,
+            secure: false, // HTTPS를 사용하는 경우 true, / HTTP 환경이므로 false로 설정
+            sameSite: "lax", // HTTP 환경에서는 'none' 대신 'lax' 사용
+            path: "/",
+            // EC2 도메인으로 접근하는 경우의 도메인 설정
+            domain: "3.38.183.123", // EC2 IP 주소 또는 도메인 주소
+          })
+          .json({
+            id: userDoc._id,
+            username,
+          });
       });
     } else {
       res.status(400).json({ message: "비밀번호가 틀렸습니다" });
@@ -61,7 +70,7 @@ const authController = {
 
   logout(req, res) {
     res.cookie("token", "").json("로그아웃 되었습니다.");
-  }
+  },
 };
 
 module.exports = authController;
